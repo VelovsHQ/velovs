99b5f6f8538ae5d705a1d50ea816a053a14999bd
Senuka Chandunu <senukaacc@gmail.com>
2025-12-14 03:30:35 +0530

    fix: navbar topline issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{/* <Navbar /> */}
 				<SmoothScroller />
 				{children}
 			</body>
